08c8b8832bed658d5edfd4a605aa8af51eb2903e
Giovanni Buracci <giovanniburacci@MacBook-Pro-di-Giovanni.local>
2024-12-29 16:43:52 +0100

    coroutines for camera

--- packages/android_app/app/src/main/java/com/example/macc_app/screens/Screen1.kt
+++ packages/android_app/app/src/main/java/com/example/macc_app/screens/Screen1.kt
@@ -40,7 +40,6 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -48,14 +47,18 @@ import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 @Composable
 fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
+
+    val coroutineScope = rememberCoroutineScope() // Get the coroutine scope
 
     var imageCapture by remember { mutableStateOf<ImageCapture?>(null) }
     var capturedImageUri by remember { mutableStateOf<Uri?>(null) }
@@ -75,25 +78,25 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
 
     var previewView by remember { mutableStateOf<PreviewView?>(null) }
 
-    fun recognizeTextFromBitmap(bitmap: Bitmap, context: Context) {
+    suspend fun recognizeTextFromBitmap(bitmap: Bitmap, context: Context) {
         // Convert the Bitmap to an InputImage
         val image = InputImage.fromBitmap(bitmap, 0)
 
         // Initialize the Text Recognizer
         val recognizer: TextRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
-        // Process the image
-        recognizer.process(image)
-            .addOnSuccessListener { visionText ->
-                // Handle the recognized text
-                showRecognizedTextDialog = true
-                recognizedText = visionText.text
-            }
-            .addOnFailureListener { exception ->
-                // Handle any error
-                exception.printStackTrace()
-                Toast.makeText(context, "Text recognition failed: ${exception.message}", Toast.LENGTH_SHORT).show()
-            }
+        try {
+            // Process the image
+            val visionText = recognizer.process(image).await()
+            // Handle the recognized text
+            showRecognizedTextDialog = true
+            recognizedText = visionText.text
+        } catch (exception: Exception) {
+            // Handle any error
+            exception.printStackTrace()
+            Toast.makeText(context, "Text recognition failed: ${exception.message}", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     // Function to convert URI to Bitmap
@@ -103,14 +106,13 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
     }
 
 
-    fun takePicture(
+    suspend fun takePicture(
         context: Context, // Current context
-        imageCapture: ImageCapture?, // ImageCapture instance for taking pictures
-        onImageCaptured: (Bitmap) -> Unit // Callback to handle the captured image as a Bitmap
-    ) {
+        imageCapture: ImageCapture? // ImageCapture instance for taking pictures
+    ): Bitmap = suspendCancellableCoroutine { continuation ->
         if (imageCapture == null) {
-            Toast.makeText(context, "Camera not ready", Toast.LENGTH_SHORT).show()
-            return // Exit if the camera is not ready
+            continuation.resumeWithException(Exception("Camera not ready"))
+            return@suspendCancellableCoroutine
         }
 
         // Use the camera to capture the image
@@ -121,23 +123,28 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
 
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(context),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    // Get image as Bitmap
-                    val savedUri = Uri.fromFile(photoFile)
-                    val bitmap = getBitmapFromUri(savedUri, context)
-                    onImageCaptured(bitmap) // Pass the bitmap to the callback
-                }
+        try {
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(context),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        // Get image as Bitmap
+                        val savedUri = Uri.fromFile(photoFile)
+                        val bitmap = getBitmapFromUri(savedUri, context)
+                        continuation.resume(bitmap) // Resume the coroutine with the bitmap
+                    }
 
-                override fun onError(exception: ImageCaptureException) {
-                    exception.printStackTrace()
-                    Toast.makeText(context, "Failed to capture image: ${exception.message}", Toast.LENGTH_SHORT).show()
+                    override fun onError(exception: ImageCaptureException) {
+                        exception.printStackTrace()
+                        continuation.resumeWithException(Exception("Failed to capture image: ${exception.message}"))
+                    }
                 }
-            }
-        )
+            )
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            continuation.resumeWithException(Exception("Failed to capture image: ${exception.message}"))
+        }
     }
 
     val galleryLauncher = rememberLauncherForActivityResult(
@@ -145,9 +152,12 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
     ) { uri: Uri? ->
         // Handle gallery selection
         uri?.let { imageUri ->
-            // Convert URI to Bitmap and perform text recognition
-            val bitmap = getBitmapFromUri(imageUri, context)
-            recognizeTextFromBitmap(bitmap, context)
+            coroutineScope.launch {
+                // Convert URI to Bitmap and perform text recognition
+                val bitmap = getBitmapFromUri(imageUri, context)
+                recognizeTextFromBitmap(bitmap, context)
+            }
+
         }
     }
     // Request camera permission on initial composition
@@ -165,10 +175,10 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
             RecognizedTextDialog(
                 onDismiss = {showRecognizedTextDialog = false; recognizedText = ""},
                 onConfirm = {
-                        viewModel.sendMessage(recognizedText, type = MessageType.TEXT, targetLanguage = "it", timestamp = System.currentTimeMillis())
-                        showRecognizedTextDialog = false;
-                        recognizedText = "";
-                        navController.navigate("screen2")
+                    viewModel.sendMessage(recognizedText, type = MessageType.TEXT, targetLanguage = "it", timestamp = System.currentTimeMillis())
+                    showRecognizedTextDialog = false;
+                    recognizedText = "";
+                    navController.navigate("screen2")
                 },
                 showDialog = showRecognizedTextDialog,
                 text = recognizedText
@@ -196,9 +206,15 @@ fun Screen1(viewModel: ChatViewModel = viewModel(), navController: NavController
         // Take Picture Button
         Button(
             onClick = {
-                takePicture(context, imageCapture) { bitmap ->
-                    // Perform text recognition on the captured Bitmap
-                    recognizeTextFromBitmap(bitmap, context)
+                coroutineScope.launch {
+                    try {
+                        val bitmap = takePicture(context, imageCapture)
+                        // Perform text recognition on the captured Bitmap
+                        recognizeTextFromBitmap(bitmap, context)
+                    } catch (e: Exception) {
+                        // Handle errors, e.g., camera not ready or failed to capture image
+                        Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
                 }
             },
             modifier = Modifier.fillMaxWidth()
